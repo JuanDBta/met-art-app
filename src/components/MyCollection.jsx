8066ee763a380321e751f1ca53ad6d3a2d5f15f8
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-21 14:33:05 -0500

    Add remove button to My Collection section

--- src/components/MyCollection.jsx
+++ src/components/MyCollection.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelected } from '../redux/features/paintings/paintingsSlice';
 import styles from '../styles/MyCollection.module.css';
 
 function MyCollection() {
+  const dispatch = useDispatch();
+
   const selectedPaintings = useSelector((state) => {
     const selected = [];
     const artistLastnames = Object.keys(state.paintings.data);
@@ -14,18 +17,36 @@ function MyCollection() {
     return selected;
   });
 
+  const handleRemovePainting = (lastname, objectId) => {
+    // Cambiar el estado isSelected a false cuando se hace clic en el botón "Remove"
+    dispatch(setSelected({ objectId, isSelected: false }));
+    // Y cualquier otra lógica que desees realizar al quitar una pintura de MyCollection
+    // ...
+  };
+
   return (
     <section className={styles.collectioncontainer}>
       <div className={styles.paintings}>
         <h2 className={styles.paintingstitle}>My Paintings</h2>
         {selectedPaintings.map((artist) => (
           <div key={artist.lastname}>
-            <h3>{artist.lastname}</h3>
             <ul className={styles.paintingslist}>
               {artist.paintings.map((painting) => (
                 <li key={painting.objectID}>
+                  <h3>{artist.lastname}</h3>
                   <h4>{painting.title}</h4>
-                  <img src={painting.primaryImage} alt={painting.title} className={styles.paintingImage} />
+                  <img
+                    src={painting.primaryImage}
+                    alt={painting.title}
+                    className={styles.paintingImage}
+                  />
+                  <button
+                    type="button"
+                    className={styles.removeButton}
+                    onClick={() => handleRemovePainting(artist.lastname, painting.objectID)}
+                  >
+                    Remove
+                  </button>
                 </li>
               ))}
             </ul>
