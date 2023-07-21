26fabb5f154debcc549781647f6a2cded33a5eed
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-21 14:21:25 -0500

    Rendering selected paintings in MyCollection section

--- src/components/MyCollection.jsx
+++ src/components/MyCollection.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from '../styles/MyCollection.module.css';
 
 function MyCollection() {
+  const selectedPaintings = useSelector((state) => {
+    const selected = [];
+    const artistLastnames = Object.keys(state.paintings.data);
+    artistLastnames.forEach((lastname) => {
+      const paintings = state.paintings.data[lastname];
+      const selectedPaintingsForArtist = paintings.filter((painting) => painting.isSelected);
+      selected.push({ lastname, paintings: selectedPaintingsForArtist });
+    });
+    return selected;
+  });
+
   return (
-    <section className={styles.profilecontainer}>
-      <div className={styles.rockets}>
-        <h2 className={styles.rocketstitle}>My Paintings</h2>
-        <ul className={styles.rocketslist}>
-          <li>
-            My first favorite painting
-          </li>
-        </ul>
+    <section className={styles.collectioncontainer}>
+      <div className={styles.paintings}>
+        <h2 className={styles.paintingstitle}>My Paintings</h2>
+        {selectedPaintings.map((artist) => (
+          <div key={artist.lastname}>
+            <h3>{artist.lastname}</h3>
+            <ul className={styles.paintingslist}>
+              {artist.paintings.map((painting) => (
+                <li key={painting.objectID}>
+                  <h4>{painting.title}</h4>
+                  <img src={painting.primaryImage} alt={painting.title} className={styles.paintingImage} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
