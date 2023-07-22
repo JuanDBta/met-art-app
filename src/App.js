a54b8071ae2ddffaf532b7c4b871ee4b0580cf64
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-22 09:19:21 -0500

    Fix NavBar removing unnnecessary elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { RiFlickrFill, RiFlickrLine } from 'react-icons/ri';
-import { CiWifiOn, CiBatteryFull, CiBluetooth } from 'react-icons/ci';
 import Logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
@@ -14,17 +12,10 @@ function App() {
     <>
       <header className="App-header">
         <div className="logo-title">
-          <RiFlickrFill className="icon1" />
-          <RiFlickrLine className="icon2" />
-          <p className="signal gill">BELL</p>
-          <CiWifiOn className="icon3" />
           <img src={Logo} className="App-logo" alt="logo" />
           <h1 className="title gill">MET</h1>
         </div>
         <NavBar className="bar" />
-        <CiBluetooth className="bluetooth" />
-        <p className="level">100%</p>
-        <CiBatteryFull className="battery" />
       </header>
       <Routes>
         <Route path="/" element={<Artists />} />
