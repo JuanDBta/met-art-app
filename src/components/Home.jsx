378d56c82913d9bc49bfb49c21e37aafbddd15fd
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-27 12:16:22 -0500

    Styling done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.css';
 
 const Home = () => {
   const info = {
-    line1: 'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random art-related quote.',
-    line2: 'You can find three sections in our webpage.  This is our welcome section.  In Calculator section you will be able to make simple calculations and in the Quote section you can find an inspirational quote to share some wisdom with you.  I hope you enjoy our site!!!',
+    line1: 'MET ART is a website for all fans of arts. It is a Single Page App (SPA) that allows users to find his favorite artist and watch their random art-related paintings.   All these paintings are available in the Metropolitan Museum of Art, New York.',
+    line2: 'You can find three sections in our webpage. In Artists section you will be able to choose your favorite artist and in the myCollection section you can save your selected paintings.  I hope you enjoy our site!!!',
   };
   return (
 
